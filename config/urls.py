734391e5b1d865821ee8f83ba9c4c85834486f27
woohwanshin <woohwan5810@likelion.org>
2020-06-20 17:55:50 +0900

    main page url change

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookmark/', include('bookmark.urls'))
+    path('', include('bookmark.urls'))
 ]
